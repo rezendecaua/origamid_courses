ab392e9e54cada7704d4e720bbfacb0bfc21f7b0
Cauã Rezende <cauagrezende@gmail.com>
2020-05-29 20:37:17 -0300

    create final project ranek

---
+++ vue/techno/app.js
@@ -0,0 +1,49 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    produtos: [],
+    produto: false,
+    carrinho: [],
+    carrinhoTotal: 0,
+  },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then((r) => r.json())
+        .then((r) => {
+          this.produtos = r;
+        });
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.produto = r;
+        });
+    },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false;
+    },
+    adicionarItem(event) {
+      console.log(event);
+    },
+  },
+  created() {
+    this.fetchProdutos();
+  },
+});
